b1f5887fdf42211102ea2ca3a564c3bea76f3e7e
wky <2270713732@qq.com>
2020-07-15 15:34:44 +0800

    初始化项目

---
+++ gmall-coupon/src/test/java/cn/wky/gmall/coupon/GmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wky.gmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-order/src/test/java/cn/wky/gmall/order/GmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wky.gmall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
